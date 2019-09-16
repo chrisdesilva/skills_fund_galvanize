1441e1140788bd12aa48b71509ec2e3c2f96099e
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-16 10:53:56 -0500

    Remove school name from apply now event

--- src/components/loanapp.js
+++ src/components/loanapp.js
@@ -122,7 +122,7 @@ const LoanApp = React.forwardRef((props, ref) => {
 
     const trackGoogleAnalyticsEvent = () => {
             ReactGA.event({
-                category: 'Apply Now Button | Galvanize',
+                category: 'Apply Now Button',
                 action: 'click',
                 label: 'submitted loan application'
             })
